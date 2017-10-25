de460b6021a6f9a06efb2647d1342b5299390588
facundo palma <fpalma2k@gmail.com>
2017-10-25 19:01:16 -0300

    avances

--- Trabajo Practico 2/conexionPop.py
+++ Trabajo Practico 2/conexionPop.py
@@ -1,5 +1,6 @@
 from poplib import *
 import threading
+import email
 
 # Variables
 
@@ -10,8 +11,10 @@ frecuencia_recepcion = None
 # Funciones
 
 def conectar_a_servidor(ssl, servidor, username, contrasenia, frecuencia, puerto=""):
-    thread = threading.Thread(target=ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, frecuencia))
+    thread = threading.Thread(target=ejecutar_conectar_a_servidor(ssl, servidor, puerto, username,
+                                                                  contrasenia, frecuencia))
     thread.start()
+
 
 def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, frecuencia):
     global user
@@ -19,7 +22,6 @@ def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, f
     try:
         user = username
         frecuencia_recepcion = frecuencia
-        mailbox = None
         if ssl:
             mailbox = POP3(host=servidor, port=puerto)
         else:
@@ -28,8 +30,7 @@ def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, f
         mailbox.user(user)
         mailbox.pass_(contrasenia)
 
-        obtener_mails(mailbox=mailbox)
-
+        procesar_emails(mailbox)
 
     except error_proto as err:
         print "Error: " + err.message
@@ -38,8 +39,43 @@ def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, f
         print "Error: " + err.message
 
 
-def obtener_mails(mailbox):
+def procesar_emails(mailbox):
+
+    obtener_emails(mailbox=mailbox)
+
+
+def obtener_emails(mailbox):
+    """
+    Traer emails del servidor y extrar los mensajes
+    :param mailbox:
+    :return:
+    """
+    mensajes_emails = []
+
     num_messages = len(mailbox.list()[1])
     for i in range(num_messages):
-        (server_msg, body, octets) = mailbox.retr(i)
-        print (body)
+        print 'Mensaje email: ' + str(i)
+
+        if corroborar_remitente():
+
+            for linea_email in mailbox.retr(i + 1)[1]:
+                # Se recorre cada linea del email y se obtiene el texto
+                cuerpo_email = email.message_from_string(linea_email)
+
+            # Se obtiene el cuerpo del email
+            mensaje_email = cuerpo_email.get_payload()
+            print mensaje_email
+            mensajes_emails.append(mensaje_email)
+        else:
+            print "Remitente invalido"
+
+    return mensajes_emails
+
+def corroborar_remitente(remitente):
+    """
+    Se comprueba que el remitente esta en el xml de remitentes habilitados
+    :param remitente:
+    :return:
+    """
+    return True
+
