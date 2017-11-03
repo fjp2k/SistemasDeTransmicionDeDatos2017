2feab70903d2535e273fd6c5e47be491b9dabd93
facundo palma <fpalma2k@gmail.com>
2017-11-02 21:14:39 -0300

    avances

--- Trabajo Practico 2/conexionPop.py
+++ Trabajo Practico 2/conexionPop.py
@@ -1,27 +1,62 @@
 from poplib import *
 import threading
 import email
+import re
+
+
+# Constantes
+
+KEY_EMAIL = 'email'
+KEY_TIMESTAMP = 'timestamp'
+KEY_DATOS = 'datos'
+KEY_TEMPERATURA = 'temperatura'
+KEY_TENSION = 'tension'
+KEY_CORRIENTE = 'corriente'
+KEY_POTENCIA = 'potencia'
+KEY_PRESION = 'presion'
+
 
 # Variables
 
+controlador_gui = None
 user = None
 frecuencia_recepcion = None
+
+proceso_analisis_de_emails = None
+detener_proceso = None
 
 
 # Funciones
 
-def conectar_a_servidor(ssl, servidor, username, contrasenia, frecuencia, puerto=""):
-    thread = threading.Thread(target=ejecutar_conectar_a_servidor(ssl, servidor, puerto, username,
-                                                                  contrasenia, frecuencia))
-    thread.start()
-
-
-def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, frecuencia):
+def conectar_a_servidor(controlador, ssl, servidor, username, contrasenia, frecuencia, puerto=""):
+    global controlador_gui
     global user
     global frecuencia_recepcion
+    global proceso_analisis_de_emails
+    global detener_proceso
+
+    controlador_gui = controlador
+    user = username
+    frecuencia_recepcion = frecuencia
+
+    # inicio hilo que trae los emails y los procesa
+    detener_proceso = threading.Event()
+    proceso_analisis_de_emails = threading.Thread(target=ejecutar_conectar_a_servidor,
+                                                  args=(ssl, servidor, puerto, contrasenia,))
+    proceso_analisis_de_emails.start()
+
+
+def desconectar():
+    controlador_gui.cambiar_mensaje_estado('Desconectando...')
+    detener_proceso.set()
+
+
+def ejecutar_conectar_a_servidor(ssl, servidor, puerto, contrasenia):
+    controlador_gui.cambiar_mensaje_estado('Conectando a servidor')
     try:
-        user = username
-        frecuencia_recepcion = frecuencia
+
+        obtener_datos_almacenados()
+
         if ssl:
             mailbox = POP3(host=servidor, port=puerto)
         else:
@@ -29,53 +64,133 @@ def ejecutar_conectar_a_servidor(ssl, servidor, puerto, username, contrasenia, f
 
         mailbox.user(user)
         mailbox.pass_(contrasenia)
+        controlador_gui.cambiar_mensaje_estado('Conectando: obteniendo tramas')
 
-        procesar_emails(mailbox)
+        while not detener_proceso.is_set():
+            procesar_emails(mailbox)
+            detener_proceso.wait(frecuencia_recepcion)
+
+        controlador_gui.cambiar_mensaje_estado('Desconectando...')
+
 
     except error_proto as err:
+        controlador_gui.cambiar_mensaje_estado('Error en conexion: '+err.message)
         print "Error: " + err.message
 
     except Exception as err:
+        controlador_gui.cambiar_mensaje_estado('Error en conexion: ' + err.message)
         print "Error: " + err.message
 
 
-def procesar_emails(mailbox):
-
-    obtener_emails(mailbox=mailbox)
-
-
-def obtener_emails(mailbox):
+def obtener_datos_almacenados():
     """
-    Traer emails del servidor y extrar los mensajes
+    Buscar las tramas leidas con anterioridad y mostrarlas por pantalla
+    :return:
+    """
+    datos_almacenados = leer_datos_bd()
+
+    for trama in datos_almacenados:
+        imprimir_trama(trama)
+
+
+def leer_datos_bd():
+    """
+    Devolver lista con cada una de las tramas almacenadas
+    :return:
+    """
+    # TODO
+    return []
+
+
+def procesar_emails(mailbox):
+    """
+    Traer emails del servidor, extrar los mensajes y procesarlos
     :param mailbox:
     :return:
     """
     mensajes_emails = []
 
     num_messages = len(mailbox.list()[1])
-    for i in range(num_messages):
-        print 'Mensaje email: ' + str(i)
+    for numero_mensaje in range(num_messages):
+        print 'Mensaje email: ' + str(numero_mensaje)
 
-        if corroborar_remitente():
+        if corroborar_remitente(""):
 
-            for linea_email in mailbox.retr(i + 1)[1]:
-                # Se recorre cada linea del email y se obtiene el texto
-                cuerpo_email = email.message_from_string(linea_email)
-
-            # Se obtiene el cuerpo del email
-            mensaje_email = cuerpo_email.get_payload()
-            print mensaje_email
-            mensajes_emails.append(mensaje_email)
+            trama_email = obtener_trama(numero_mensaje, mailbox)
+            if trama_email is not None:
+                diccionario_datos = extraer_datos_trama(trama_email)
+                mensajes_emails.append(mensaje_email)
         else:
             print "Remitente invalido"
 
     return mensajes_emails
 
+
 def corroborar_remitente(remitente):
     """
-    Se comprueba que el remitente esta en el xml de remitentes habilitados
+    Se comprueba que el remitente esta en el json de remitentes habilitados
     :param remitente:
-    :return:
+    :return: devuelve verdadero si esta el remitente esta en el json
     """
+
     return True
 
+
+def obtener_trama(numero_mensaje, mensajes):
+    for linea_email in mensajes.retr(numero_mensaje + 1)[1]:
+        # Se recorre cada linea del email y se comprueba si es una trama
+        trama = corroborar_trama(linea_email)
+        if trama is not None:
+            return trama
+    return None
+
+
+def corroborar_trama(mensaje_email):
+    try:
+        s = mensaje_email
+        trama = re.search('>.+?<', s)
+
+        if trama:
+            return trama.group()
+        else:
+            return None
+
+    except Exception as err:
+        return None
+
+def extraer_datos_trama(trama_email):
+
+
+
+
+
+
+def guardar_datos_trama(diccionario_trama):
+    """
+    Guardar los datos de una trama en el json
+    :param trama:
+    :return:
+    """
+    # TODO
+
+
+def imprimir_trama(diccionario_datos):
+    """
+    A partir del diccionario de una trama, imprimir por pantalla los resultados
+    :param diccionario_datos:
+    :return:
+    """
+    # TODO
+
+    timestamp = ''
+    temperatura = ''
+    tension = ''
+    corriente = ''
+    potencia = ''
+    presion = ''
+
+    string_a_imprimir = 'Timestamp: ' + timestamp + ' -> Temperatura: ' + temperatura \
+                        + ' / Tension: ' + tension +  ' / Corriente: ' + corriente \
+                        + ' / Potencia: ' + potencia + ' / Presion: ' + presion
+
+    controlador_gui.imprimir(string_a_imprimir)
--- Trabajo Practico 2/controlador.py
+++ Trabajo Practico 2/controlador.py
@@ -18,8 +18,9 @@ class Controlador():
     def __init__(self, gui):
         self.gui = gui
 
-    def configurar_gui(self):
+        self.index_textbox = 0
 
+    def configurar_gui(self):
         """
         self.gui.conectar_btn
         self.gui.desconectar_btn
@@ -36,14 +37,26 @@ class Controlador():
 
         self.gui.info_listbox.delete(0, END)
         """
+        print "Configurando GUI"
 
     def conectar(self):
-        conexionPop.conectar_a_servidor(ssl=False, servidor="localhost",
-                                        username="test", contrasenia="123456", frecuencia=0)
+        conexionPop.conectar_a_servidor(controlador=self,
+                                        ssl=False,
+                                        servidor="localhost",
+                                        username="test",
+                                        contrasenia="123456",
+                                        frecuencia=0)
 
+    def desconectar(self):
+        conexionPop.desconectar()
+
+    def cambiar_mensaje_estado(self, mensaje):
+        self.gui.estado_conexion_info.configure(text=''+mensaje+'')
+
+    def limpiar_pantalla(self):
+        self.gui.info_listbox.delete(0, END)
+        self.index_textbox = 0
 
     def imprimir(self, mensaje):
-        self.gui.info_listbox.delete(0, END)
-
-        self.gui.info_listbox.insert(1,mensaje)
-
+        self.gui.info_listbox.insert(self.index_textbox, mensaje)
+        self.index_textbox += 1
