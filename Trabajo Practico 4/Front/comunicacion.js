9d78219e3efc98bcfadec1e1867c8bd234dc5243
facundocianciop <facundocianciop@gmail.com>
2017-11-19 21:06:38 -0300

    cambio de nombre metodos

--- Trabajo Practico 4/Front/comunicacion.js
+++ Trabajo Practico 4/Front/comunicacion.js
@@ -1,33 +1,16 @@
-window.idUltimaPeticion = 1;
+window.idUltimaTrama = 0;
 //window.json = "{\"mensajes\":[{\"tension\":\"1\"}],\"idUltimaMedicion\":\"2\"}";
-window.json = {"mensajes":[{"timestamp":"16:03","tension":"1","potencia":"2","corriente":"3","presion":"5","temperatura":"13","remitente":"pablo"}],"idUltimaPeticion":"2"};
 $(document).ready(
-    (function local(){
-         alert(json.idUltimaPeticion);
-         $.each(json.mensajes, function(key,value){
-                alert ("tension" + value.tension);
-                fila ="<tr>"+"<td>"+value.timestamp+"</td>"
-                +"<td>"+value.remitente+"</td>"
-                +"<td>"+value.temperatura+"</td>"
-                +"<td>"+value.tension+"</td>"
-                +"<td>"+value.corriente+"</td>"
-                +"<td>"+value.potencia+"</td>"
-                +"<td>"+value.presion+"</td>"
-                +"</tr>";
-                alert(fila);
-                document.getElementById("demo").innerHTML = "Paragraph changed!";
-                $("#demo").html('<p>chau</p>');
-                $('#tabla tbody').append(fila);
-            });
-            idUltimaPeticion=json.idUltimaPeticion;//acá pensé q en el json que mandás sea con la estructura {"mensajes":[{"tension":"1",....}],"idUltimaPeticion":"1"}
-                })(),
     (function llenarTabla(){
         $.ajax({
-               url: 'D:\Facultad\QuintoAnio\TransmisionDeDatos\TP5\testData.json', //acá tiene que ir la url de la llamada para traer todos los datos
-               dataType: "json",  // <---- For example
+               url: 'http://localhost:5000/getTramas', 
+               type: "GET",
+               headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
                success: function(data){
                  alert("exito");
-                 $.each(data.mensajes, function(key,value){
+                 $.each(data, function(key,value){
                 fila = "<tr>"+"<td>"+value.timestamp+"</td>"
                 +"<td>"+value.remitente+"</td>"+
                 +"<td>"+value.temperatura+"</td>"+
@@ -36,9 +19,12 @@ $(document).ready(
                 +"<td>"+value.potencia+"</td>"+
                 +"<td>"+value.presion+"</td>"+
                 "</tr>";
+                if (value.id > idUltimaTrama){
+                        idUltimaTrama = value.id;
+                }
+                alert(idUltimaTrama);
                 $("#tabla").append(fila);
-                });
-                idUltimaPeticion=data.idUltimaPeticion;//acá pensé q en el json que mandás sea con la estructura {"mensajes":[{"tension":"1",....}],"idUltimaPeticion":"1"}
+                })
                 },
                error: function(response){
                    alert("error al llenar tabla"); // <------ If errors occurs trigger an alert
@@ -47,9 +33,11 @@ $(document).ready(
         )(),
     (function actualizar(){
         $.ajax({
-                url: 'D:\Facultad\QuintoAnio\TransmisionDeDatos\TP5\testData.json', //acá tiene q ir la url de la llamada actualizar
+                url: 'http://localhost:5000/getUltimasTramas', //acá tiene q ir la url de la llamada actualizar
+                dataType:"json",
+                type: "POST",
                 data: { 
-                        idUltimaPeticion: window.idUltimaPeticion
+                        id: window.idUltimaTrama
                         },
                 success: function(data){
                  alert("exito");
@@ -64,10 +52,10 @@ $(document).ready(
                 "</tr>";
                 $("#tabla").append(fila);
                  });
-                idUltimaPeticion=data.idUltimaPeticion;//guardo la última petición
+                idUltimaTrama=data.idUltimaTrama;//guardo la última petición
              },
                 error: function(data){
-                    alert("error al actualizar: "+ window.idUltimaPeticion);
+                    alert("error al actualizar: "+ window.idUltimaTrama);
                 },
                 complete: function() {
                 // Schedule the next request when the current one's complete
